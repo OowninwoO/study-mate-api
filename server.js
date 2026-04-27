3fe9272ec7ab8e70ad1db9a4c86462719e918331
niw0906 <niw0906@naver.com>
2026-04-27 21:29:46 +0900

    feat: server.js 유저 라우트 추가

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const quizRoutes = require('./routes/quiz_routes');
+const userRoutes = require('./routes/user_routes');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/quiz', quizRoutes);
+app.use('/user', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
