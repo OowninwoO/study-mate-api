3d5d673248c89f0711ae69dbcefaf350841fa9b2
niw0906 <niw0906@naver.com>
2026-04-27 23:23:59 +0900

    fix: pg int 파싱 추가

--- db.js
+++ db.js
@@ -1,4 +1,8 @@
-const { Pool } = require('pg');
+const { Pool, types } = require('pg');
+require('dotenv').config();
+
+// BIGINT(int8)는 pg에서 기본적으로 문자열로 오므로, 응답에서 number로 쓰기 위해 int로 파싱한다.
+types.setTypeParser(20, (val) => (val === null ? null : parseInt(val, 10)));
 
 const pool = new Pool({
   host: process.env.PGHOST,
@@ -6,6 +10,7 @@ const pool = new Pool({
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
   port: process.env.PGPORT,
+  // ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
